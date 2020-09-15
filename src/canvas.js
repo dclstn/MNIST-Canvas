aa1aea4bc9eedace421af48fff8664783553ce7e
Vasp1 <43322006+Vasp1@users.noreply.github.com>
2020-09-15 05:33:23 +0100

    ✨ First commit

---
+++ src/canvas.js
@@ -0,0 +1,109 @@
+
+var multiplier = 10
+
+var clear = function clear() {
+    let canvas = document.querySelector('#canvas');
+    let ctx = canvas.getContext('2d');
+    let small_canvas = document.querySelector('#small-canvas');
+    let small_ctx = small_canvas.getContext('2d');
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    small_ctx.fillStyle = "white";
+    small_ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+$(window).on("load", () => {
+
+    var canvas = document.querySelector('#canvas');
+    var ctx = canvas.getContext('2d');
+
+    canvas.height = 28 * multiplier;
+    canvas.width  = 28 * multiplier;
+
+    var small_canvas = document.querySelector('#small-canvas');
+    var small_ctx = small_canvas.getContext('2d');
+
+    small_canvas.height = 28;
+    small_canvas.width = 28;
+
+    clear();
+    drawingInit();
+    
+    $('#clear').on('click', clear);
+
+    function drawingInit() {
+        $('#canvas').on('mousemove', (event) => findXY('move', event));
+        $('#canvas').on('mousedown', (event) => findXY('down', event));
+        $('#canvas').on('mouseup', (event) => findXY('stop', event));
+        $('#canvas').on('mouseout', (event) => findXY('stop', event));
+    }
+    
+    function draw(prevX, prevY, currX, currY) {
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(currX, currY);
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 2 * multiplier;
+        ctx.lineCap = 'round';
+        ctx.stroke();
+        ctx.closePath();
+
+        small_ctx.beginPath();
+        small_ctx.moveTo(prevX / multiplier, prevY / multiplier);
+        small_ctx.lineTo(currX / multiplier, currY / multiplier);
+        small_ctx.strokeStyle = 'black';
+        small_ctx.lineWidth = 2;
+        small_ctx.lineCap = 'round';
+        small_ctx.stroke();
+        small_ctx.closePath();
+
+        predictThis(small_ctx);
+    }
+
+    var flag = false;
+    var currX = 0;
+    var currY = 0;
+    var prevX = 0;
+    var prevY = 0;
+    
+    function findXY(res, e) {
+        switch (res) {
+            case 'down':
+                prevX = currX;
+                prevY = currY;
+                currX = e.clientX - canvas.offsetLeft;
+                currY = e.clientY - canvas.offsetTop;
+                flag = true;
+                break;
+            case 'stop':
+                flag = false;
+                break;
+            case 'move':
+                if (flag) {
+                    prevX = currX;
+                    prevY = currY;
+                    currX = e.clientX - canvas.offsetLeft;
+                    currY = e.clientY - canvas.offsetTop;
+                    draw(prevX, prevY, currX, currY);
+                }
+    
+        }
+    }
+
+})
+
+var setStatus = function(status) {
+    var textData = document.querySelector('#status');
+    textData.textContent = '✏️ Training: ' + status
+
+    
+}
+
+var setPrediction = function(status) {
+    var textData = document.querySelector('#prediction');
+    textData.textContent = '📝 Prediction: ' + status
+}
+
+
+
+
---
+++ src/train.js
@@ -0,0 +1,79 @@
+var training = false;
+
+var model = tf.sequential();
+
+model.add(tf.layers.dense({ units: 32, inputShape: [784], activation: 'sigmoid' }));     // Hidden layer
+model.add(tf.layers.dense({ units: 32, inputShape: [32], activation: 'sigmoid' }));     // Hidden layer
+model.add(tf.layers.dense({ units: 10, inputShape: [32], activation: 'sigmoid' }));     // Output layer
+
+const sgdOut = tf.train.sgd(0.5) // Squared gradient decent
+
+model.compile({
+    optimizer: sgdOut,
+    loss: 'meanSquaredError',
+    metrics=['accuracy']
+})
+
+$(window).on('load', () => {
+
+    setPrediction(`draw something!`)
+
+    var epoch = 0;
+    $('#stop').hide();
+
+    $('#stop').on('click', () => {
+        training = false;
+        $('#stop').hide();
+        $('#train').show();
+    })
+    
+    $('#train').on('click', () => {
+        $('#stop').show();
+        $('#train').hide();
+        setStatus("Starting training...");
+        training = true;
+
+        var set = mnist.set(10000, 0)
+        var trainingSet = set.training;
+
+        const xs = tf.tensor2d(trainingSet.map(data => data.input))
+        const ys = tf.tensor2d(trainingSet.map(data => data.output))
+
+        async function train(xs = tf.tensor2d, ys = tf.tensor2d) {
+            while (training) {
+                epoch++
+                await model.fit(xs, ys, {
+                    verbose: false,
+                    shuffle: true
+                }).then((result) => {
+                    let loss = result.history.loss
+                    console.log(result)
+                    setStatus(`Epoch ${epoch} - Loss ${loss}`)
+                });
+            }
+        }
+        
+        train(xs, ys).then(() => {
+            setStatus(`Training Completed!`)
+            training = false;
+        })
+    })
+})
+
+
+
+var predictThis = function (ctx) {
+    const data = ctx.getImageData(0, 0, canvas.width / multiplier, canvas.height / multiplier).data;
+
+    let out_data = [];
+    for (var i = 0; i < data.length; i = i + 4) {
+        out_data.push(pixel = data[i] == 255 ? 0 : 1)
+    }
+    const xs = tf.tensor2d([out_data])
+    const out = model.predict(xs)
+    let index = out.argMax(1).dataSync()[0];
+    setPrediction(`${numbers[index]}`)
+    
+}
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
